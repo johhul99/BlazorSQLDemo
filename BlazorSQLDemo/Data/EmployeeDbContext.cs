9ee6b685ad655503f1b8ded13e979336cfe34826
Johan Hultin <johan.hultin99@gmail.com>
2025-02-12 11:23:33 +0100

    Add project files.

---
+++ BlazorSQLDemo/Data/EmployeeDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BlazorSQLDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSQLDemo.Data
+{
+    public class EmployeeDbContext : DbContext    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+        
+    }
+}
---
+++ BlazorSQLDemo/Data/EmployeeService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorSQLDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSQLDemo.Data
+{
+    public class EmployeeService
+    {
+        private readonly EmployeeDbContext _Context;
+
+        public EmployeeService(EmployeeDbContext context)
+        {
+            _Context = context;
+        }
+
+        public async Task AddEmployee(Employee employee)
+        {
+            try
+            {
+                _Context.Employees.Add(employee);
+                await _Context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+
+            return await _Context.Employees.ToListAsync();
+        }
+    }
+}
